6309b2c3c5810af4a118bd8987c2b3c681fb27f8
Aubs <aubs@Zoes-iMac.local>
2017-01-24 13:07:27 -0800

    First Commit

---
+++ Assets/COMLower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class COMLower : MonoBehaviour {
+    Vector3 com;
+    Rigidbody rb;
+	public float comOffeset;
+
+	public GameObject cube;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+		com = new Vector3(rb.transform.position.x, rb.transform.position.y + comOffeset, rb.transform.position.z);
+        rb.centerOfMass = com;
+		cube.transform.position = com;
+    }
+}
---
+++ Assets/SimpleCarController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo {
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor;
+	public bool steering;
+	public bool braking;
+}
+     
+public class SimpleCarController : MonoBehaviour {
+	public List<AxleInfo> axleInfos; 
+	public float maxMotorTorque;
+	public float maxSteeringAngle;
+	public float maxBrakeTorque;
+	
+	// finds the corresponding visual wheel
+	// correctly applies the transform
+	public void ApplyLocalPositionToVisuals(WheelCollider collider)
+	{
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+	
+		Transform visualWheel = collider.transform.GetChild(0);
+	
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+	
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation;
+	}
+	
+	public void FixedUpdate()
+	{
+		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		float braking = maxBrakeTorque * Input.GetAxis("Jump");
+	
+		foreach (AxleInfo axleInfo in axleInfos) {
+			if (axleInfo.steering) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) {
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+			if (axleInfo.braking){
+				axleInfo.leftWheel.brakeTorque = braking;
+				axleInfo.rightWheel.brakeTorque = braking;
+			}
+			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+		}
+	}
+}
